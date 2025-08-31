32812da925ad824f951b49d55cb1bcdb95ca7667
Jorge Mora <jmora2ec@gmail.com>
2025-08-31 12:48:36 -0500

    Connect the frontend and backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -10,6 +10,12 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello world!');
 });
 
+app.get('/api/hello', (req: Request, res: Response) => {
+  res.json({
+    message: 'Hello World!',
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
